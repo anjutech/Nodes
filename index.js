771120b681bdef3031403381faeacdc27e86e000
Anjani Nagar <anjunagar2000@gmail.com>
2024-11-13 17:19:34 +0530

    first Commit

--- index.js
+++ index.js
@@ -5,11 +5,22 @@ const app = express();
 const cors = require('cors');
 const PORT = process.env.PORT || 5000;
 
-const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/users";
 const dbName = process.env.DB_NAME;
 const client = new MongoClient(uri);
 
 app.use(cors());
+ async (req, res) => {
+  if (req.url === '/favicon.ico') {
+    return res.status(204).end(); // No content response
+  }
+
+  // Your regular function code here
+  if (req.url === '/backend') {
+    res.json(users);
+    res.status(200).json({ message: "Hello, world!" });
+  }
+};
 
 async function connectToDB() {
   try {
@@ -18,6 +29,7 @@ async function connectToDB() {
     const database = client.db(dbName);
     const collection = database.collection("users");
     const users = await collection.find({}).toArray();
+    console.log(users)
     console.log("Users Fetched Successfully!");
   } catch (err) {
     console.error("Error connecting to the database:", err);
